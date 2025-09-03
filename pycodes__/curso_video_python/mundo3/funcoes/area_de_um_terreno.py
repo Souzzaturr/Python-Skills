d146bd681430a1c2271408c29ea968c1156ddce3
Arttur Souzza <souzzaaturr@gmail.com>
2025-09-03 11:42:12 -0300

    Praticando conceitos aula 21 funcoes parte2 python mundo3 Gustavo Guanabara

--- pycodes__/curso_video_python/mundo3/funcoes/area_de_um_terreno.py
+++ pycodes__/curso_video_python/mundo3/funcoes/area_de_um_terreno.py
@@ -1,15 +1,29 @@
 #funções
 
 def area (largura: float, comprimento: float) -> float:
+  '''
+  Recebe 2 números inteiros e multiplica, retornando uma área;
+  Largura: Recebe valor inteiro representando a largura do terreno;
+  Comprimento: Recebe valor inteiro representando o comprimento do terreno;
+  Retorna -> produto entre largura e comprimento. 
+  '''
   return largura * comprimento
 
 def bloco (menssagem: str) -> str:
+  """
+  Recebe um texto de uma linha, e o envolve por um retangulo de traçinhos '-';
+  menssagem: Recebe uma string de até uma linha;
+  Retorna -> um bloco com a menssagem dentro."""
   print("+----" + "-" * len(menssagem) + "----+")
   print(f"|    {menssagem}    |")
   print("+----" + "-" * len(menssagem) + "----+")
   return
 
 #programa principal
+
+print(help(area))
+
+print(help(bloco))
 
 bloco("Controle de terrenos")
 
