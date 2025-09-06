c3c27ff445034c689f7c82fafe1401ac77477cd8
Arttur Souzza <souzzaaturr@gmail.com>
2025-09-06 08:47:34 -0300

    Reduzindo o código da função 'eh_float' do pacote 'dado' que está no pacote 'utilidadesCeV'

--- pycodes__/curso_video_python/mundo3/modulos_e_pacotes/exercitando_modulos/utilidadesCeV/dado/__init__.py
+++ pycodes__/curso_video_python/mundo3/modulos_e_pacotes/exercitando_modulos/utilidadesCeV/dado/__init__.py
@@ -2,12 +2,12 @@
 
 def eh_float (caracteres: str) -> bool:
     """Recebe -> um conjunto de caracteres do timpo string;
-    Verificação: SE esse caractere é um número OU se tem mais de 1 caractere E (caso negativo: se há apenas 1 sinal "-" E esse sinal é o primeiro caractere E o resto dos caracteres excluindo ponto e virgula, são numéricos) OU (se positivo: se não há sinal "-" e o resto dos caracteres excluindo ponto e virgula, são numéricos) E se há ponto e virgula, (apenas um, e ou só ponto ou só virgula) OU não tem nem ponto nem vírgula.;
+    Verificação: SE esse caractere é um número OU se tem mais de 1 caractere E (caso negativo: se há apenas 1 sinal "-" E esse sinal é o primeiro caractere) OU (se positivo: se não há sinal "-") E todos os caracteres que não são "-" "," "." formam números E se há ponto e virgula, (apenas um, e ou só ponto ou só virgula) OU não tem nem ponto nem vírgula.;
     Retorna -> Um valor booleano [True/False]."""
 
     caracteres = caracteres.replace(" ", "")
 
-    verificacao = (caracteres.isnumeric() or len(caracteres) > 1 and (("-" in caracteres and caracteres.count("-") == 1 and caracteres[0] == "-" and caracteres[1:].replace(".", "").replace(",", "").isnumeric()) or (not "-" in caracteres and caracteres.replace(".", "").replace(",", "").isnumeric())) and ((caracteres.count(".") == 1 and caracteres.count(",") == 0) or (caracteres.count(",") == 1 and caracteres.count(".") == 0) or (caracteres.count(".") == 0 and caracteres.count(",") == 0)))
+    verificacao = (caracteres.isnumeric() or len(caracteres) > 1 and (("-" in caracteres and caracteres.count("-") == 1 and caracteres[0] == "-") or not "-" in caracteres) and caracteres.replace("-", "").replace(".", "").replace(",", "").isnumeric() and ((caracteres.count(".") == 1 and caracteres.count(",") == 0) or (caracteres.count(",") == 1 and caracteres.count(".") == 0) or (caracteres.count(".") == 0 and caracteres.count(",") == 0)))
 
     return verificacao
 
