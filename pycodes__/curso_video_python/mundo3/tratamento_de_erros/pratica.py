063e905366f5b1680b47805a420e8b5817b1c843
Arttur Souzza <souzzaaturr@gmail.com>
2025-09-09 10:57:34 -0300

    Corrige código de 'praica.py' da aula de tratamento de erros, mundo3 python Gustavo Guanabara

--- pycodes__/curso_video_python/mundo3/tratamento_de_erros/pratica.py
+++ pycodes__/curso_video_python/mundo3/tratamento_de_erros/pratica.py
@@ -2,24 +2,24 @@ try:
     a = int(input("Digite um número inteiro: "))
     b = int(input("Digite um número inteiro: "))
 
-    try:
-        c = a / b
-        print(f"Divisão de {a} por {b} = {c}")
-    
-    except Exception as erro:
-        print("Não é possível realizar operação -> Divisão por zero!!")
-        print(f"Código do erro: {erro.__class__}")
-
 except Exception as erro:
     print("São permitidos apenas números inteiros!!")
     print(f"Código do erro: {erro.__class__}")
 
 else:
-    print("Só sucesso")
+    try:
+        c = a / b
+        print(f"Divisão de {a} por {b} = {c}")
+        print("Só sucesso")
 
-    print("Resto do código...")
+        print("Resto do código...")
 
-    print("...")
+        print("...")
+    
+    except Exception as erro:
+        print("Não é possível realizar operação -> Divisão por zero!!")
+        print(f"Código do erro: {erro.__class__}")
+        
 
 finally:
     print("--FIM DO PROGRAMA--")
